412da6b179bcdad4c4ece065137b0b08d5b97511
moonsunmi <celebmoon@naver.com>
2022-08-31 20:55:17 +0900

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
 from pybo.views import base_views
-from django.conf import settings
 import logging
 
 logging.debug('Debug Message')
@@ -12,9 +11,3 @@ urlpatterns = [
     path('common/', include('common.urls')),
     path('', base_views.index, name='index'),
 ]
-
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns += [
-        path('__debug__/', include(debug_toolbar.urls)),
-    ]
